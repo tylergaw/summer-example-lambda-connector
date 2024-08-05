6f78cced33aa50dc1f5703c6479471b5434d4683
Tyler Gaw <me@tylergaw.com>
2024-08-05 12:54:06 -0400

    Updates lambda to pull real data

--- london_metro/app.py
+++ london_metro/app.py
@@ -1,7 +1,44 @@
 import json
+import requests
+import logging
+
+# Set up logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
-def lambda_handler(event, context):
-    ret = {"state": {}, "schema": {}, "insert": {}, "hasMore": False}
+def lambda_handler(req, context):
+    logger.info("Received request: %s", json.dumps(req))
+    result = requests.get(
+        "https://api.tfl.gov.uk/line/mode/tube/status",
+        headers={"content-type": "application/json", "charset": "utf-8"},
+    )
 
-    return json.dumps(ret)
+    since_id = None
+    timeline = json.loads(result.text)
+
+    tflLineStatus = []
+    for t in timeline:
+        # Remember the first id we encounter, which is the most recent
+        if since_id == None:
+            since_id = t["id"]
+
+        # Add all tflLineStatus
+        tflLineStatus.append(
+            {
+                "linename": t["id"],
+                "status": t["lineStatuses"][0]["statusSeverityDescription"],
+                "timestamp": t["created"],
+            }
+        )
+
+    if since_id == None:
+        since_id = req.state.since_id
+    ans = {
+        # Remember the most recent id, so our requests are incremental
+        "state": {since_id: since_id},
+        "schema": {"line_status": {"primary_key": ["linename"]}},
+        "insert": {"line_status": tflLineStatus},
+        "hasMore": False,
+    }
+    return json.dumps(ans)
